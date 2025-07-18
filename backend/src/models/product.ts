5e73867ad4014a6868d544a901c9e8792c601a82
eminur <eminur@mail.ru>
2025-07-18 15:51:54 +0500

    добавлен картинки и схема модели

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -1,1 +1,48 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+interface IImage {
+  fileName: string;
+  originalName: string;
+}
+
+interface IProduct {
+  description: string;
+  image: IImage;
+  title: string;
+  category: string;
+  price: number;
+}
+
+const imageSchema = new mongoose.Schema<IImage>({
+  fileName: {
+    type: String,
+    required: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
+});
+
+const productSchema = new mongoose.Schema<IProduct>({
+  description: {
+    type: String,
+  },
+  image: imageSchema,
+  title: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    default: null,
+  },
+});
+
+export default mongoose.model<IProduct>("product", productSchema);
