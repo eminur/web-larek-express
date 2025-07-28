029efbfed68d0a2084acf75ed41825e4734bd13b
eminur <eminur@mail.ru>
2025-07-28 13:35:00 +0500

    отработаны замечания: доработать валидацию

--- backend/src/middlewares/validation.ts
+++ backend/src/middlewares/validation.ts
@@ -1,9 +1,15 @@
 // middlewares/validators.ts
-import { celebrate, Joi, Segments } from 'celebrate';
+import { celebrate, Joi, Segments } from "celebrate";
 
 export const validateCreateProduct = celebrate({
-    [Segments.BODY]: Joi.object().keys({
-        title: Joi.string().min(2).max(30).required(),
-        category: Joi.string().required(),
+  [Segments.BODY]: Joi.object().keys({
+    description: Joi.string(),
+    image: Joi.object().keys({
+      fileName: Joi.string().required(),
+      originalName: Joi.string().required(),
     }),
+    title: Joi.string().min(2).max(30).required(),
+    category: Joi.string().required(),
+    price: Joi.number().default(null),
+  }),
 });
